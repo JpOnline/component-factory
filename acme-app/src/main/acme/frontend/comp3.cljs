a3199fb31c07470f4b065cceb0fca32591ae9315
João Paulo Soares <JpSoares106@gmail.com>
2020-11-15 07:27:06 -0300

    2020-11-15

--- acme-app/src/main/acme/frontend/comp3.cljs
+++ acme-app/src/main/acme/frontend/comp3.cljs
@@ -1,31 +1,13 @@
 (ns acme.frontend.comp3
   (:require
-    [reagent.dom]
     [re-frame.core :as re-frame]
+    [acme.frontend.util :as util :refer [<sub >evt]]
     ))
 
-;; Redef re-frame subscribe and dispatch for brevity
-(def <sub (comp deref re-frame.core/subscribe))
-(def >evt re-frame.core/dispatch)
-
-(defonce already-defined-components (atom {})) ;; This is used for hot reloading.
-(defn define-custom-element! [name view-component]
-  (if (js/window.customElements.get name)
-    (swap! already-defined-components #(assoc % name view-component))
-    (let [set-shadow (fn [^js obj] (do (set! (.-shadow obj) (.attachShadow obj #js {:mode "open"})) obj))
-          _ (swap! already-defined-components #(assoc % name view-component))
-          render (fn [^js obj] (do (reagent.dom/render [(@already-defined-components name)] (.-shadow obj)) obj))
-          ;; defines the constructor function, which is the "class" object used by the customElements api
-          component (fn component []
-                      (-> (js/Reflect.construct js/HTMLElement #js [] component)
-                          set-shadow
-                          render))]
-      (set! (.-prototype component)
-            ;; establishes prototype hierarchy
-            (js/Object.create (.-prototype js/HTMLElement) #js {}))
-
-      ;;finally, defines the component with these values
-      (js/window.customElements.define name component))))
+(defn counted
+  [app-state]
+  (update-in app-state [:counter] inc))
+(re-frame/reg-event-db :counted counted)
 
 (defn component-x2 []
   [:button
@@ -33,5 +15,7 @@
    "inc"])
 
 (defn ^:dev/after-load my-comp []
-  (define-custom-element! "component-x2" component-x2)
+  (util/define-custom-element! {:element-name "component-x2"
+                                :view-component component-x2
+                                })
   )
